e6f603081130b50dcbc0a6687e75bea8a499722d
Virajtapase <virajtapasee@gmail.com>
2025-12-31 18:33:17 +0530

    New Project added

--- src/main/java/utilities/DriverLaunch.java
+++ src/main/java/utilities/DriverLaunch.java
@@ -2,14 +2,23 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverLaunch {
 
 	public WebDriver driver;
 	
 	public WebDriver driverup() {
-		 driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");   // REQUIRED
+        options.addArguments("--no-sandbox");     // REQUIRED
+        options.addArguments("--disable-dev-shm-usage"); // REQUIRED
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        return driver;
 		
-		return driver; 
 	}
 }
